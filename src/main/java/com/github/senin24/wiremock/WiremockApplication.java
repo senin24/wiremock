55580da241fa7d9fbb9f68af0733740f089f5442
Pavel_Senin <psenin@jtconsulting.ru>
2019-07-26 18:50:22 +0300

    Initial commit

---
+++ src/main/java/com/github/senin24/wiremock/WiremockApplication.java
@@ -0,0 +1,16 @@
+package com.github.senin24.wiremock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiremockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiremockApplication.class, args);
+
+
+
+	}
+
+}
---
+++ src/test/java/com/github/senin24/wiremock/WiremockApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.senin24.wiremock;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WiremockApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
